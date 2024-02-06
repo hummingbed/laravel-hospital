e10b35bbf3eddea59bc069b15aa7ee9622cd27df
MichaelMenebraye <michael@dufuna.org>
2024-02-06 12:38:47 +0100

    added auth login regsistration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/',[HomeController::class, 'index']);
+Route::get('/home',[HomeController::class, 'redirect']);
+
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
